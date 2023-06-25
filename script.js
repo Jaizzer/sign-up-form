034af6cf21a24d97b41911a85da5df9c8c273f59
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-06-25 13:46:47 +0800

    Add function that displays corresponding error in input elements

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 // Store all input elements to an array.
 const inputElements = Array.from(document.querySelectorAll('input'));
+
+// Show error in inputs.
+function showError(input, errorMessagePlaceholder) {
+
+    // If field is left empty, show respective message.
+    if (input.validity.valueMissing) {
+        errorMessagePlaceholder.textContent = "Do not live this input blank."
+    }
+    // If input is of wrong type, show respective message.
+    else if (input.validity.typeMismatch) {
+        errorMessagePlaceholder.textContent = "You need to input of type that matches the type required."
+    }
+}
+
