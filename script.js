ab6fd4a35f92b58c1fee8a9037a942f40e51d139
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-06-28 08:09:32 +0800

    Edit error messages

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function showError(input, errorMessagePlaceholder) {
 
     // If field is left empty, show respective message.
     if (input.validity.valueMissing) {
-        errorMessagePlaceholder.textContent = "Do not live this input blank."
+        errorMessagePlaceholder.textContent = "Don't leave this input blank."
     }
     // If input is of wrong type, show respective message.
     else if (input.validity.typeMismatch) {
-        errorMessagePlaceholder.textContent = "You need to input of type that matches the type required."
+        errorMessagePlaceholder.textContent = "Invalid input. Please enter a valid value."
     }
 }
 
